6e5ed5829abec786b621b3772fd5af7a035ca805
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-03 11:19:26 +0800

    Получилась рабочая программа, ошибки и недоработки - не исключены

--- FirstFormOfSecondLaba/Form1.Designer.cs
+++ FirstFormOfSecondLaba/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            button = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(54, 380);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(152, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // button
+            // 
+            button.Location = new Point(235, 380);
+            button.Name = "button";
+            button.Size = new Size(94, 29);
+            button.TabIndex = 1;
+            button.Text = "Результат";
+            button.UseVisualStyleBackColor = true;
+            button.Click += button_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(604, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Установить, является ли последовательность цифр при просмотре их слева направо";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(584, 20);
+            label2.TabIndex = 3;
+            label2.Text = "упорядоченной по возрастанию. Например, для последовательности 1,4,7,8 ответ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(475, 20);
+            label3.TabIndex = 4;
+            label3.Text = "положительный, для чисел 1,7,8,2 и 1,6,6,8 — отрицательный и т. п.";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(55, 344);
+            label4.Name = "label4";
+            label4.Size = new Size(199, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Введите число для анализа";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- FirstFormOfSecondLaba/Form1.cs
+++ FirstFormOfSecondLaba/Form1.cs
@@ -6,5 +6,67 @@ namespace FirstFormOfSecondLaba
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string message = Logic.IsDigitMessage(this.textBox1.Text);
+            MessageBox.Show(message);
+        }
+    }
+
+    public class Logic
+    {
+        public static bool Check(string smth)
+        {
+            bool forif = true;
+            for (int i = 1; i < smth.Length; i++)
+            {
+                if ((int)Char.GetNumericValue(smth[i - 1]) < (int)Char.GetNumericValue(smth[i]))
+                {
+                    forif = true;
+                }
+                else
+                {
+                    forif = false;
+                    break;
+                }
+            }
+            return forif;
+        }
+
+        public static string Message(string smth)
+        {
+            string message = "";
+
+            if (Logic.Check(smth))
+            {
+                message = "Последовательность: " + smth + " положительна!";
+            }
+            else
+            {
+                message = "Последовательность: " + smth + " отрицательна!";
+            }
+            return message;
+        }
+
+        public static string IsDigitMessage(string smth)
+        {
+            string message;
+
+            if (smth.All(char.IsDigit))
+            {
+                message = Logic.Message(smth);
+            }
+            else
+            {
+                message = "Введённая строка не являются цифрой";
+            }
+            return message;
+        }
     }
 }
